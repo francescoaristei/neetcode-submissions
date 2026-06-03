701512ca58ac28d28636bbf2454c623524a3ff51
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-06-03 15:18:33 +0200

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,56 @@
+/*
+class Solution {
+    List<List<Integer>> results = new ArrayList<>();
+    List<Integer> result = new ArrayList<>();
+
+    private void backtrack(int[] nums, int target, int idx, int sum) {
+        if (idx < nums.length) {
+            if (sum == target) {
+                List<Integer> temp = new ArrayList<>(result);
+                results.add(temp);
+            }
+            if (sum < target) {
+                result.add(nums[idx]);
+                backtrack(nums, target, idx, sum + nums[idx]);
+            }
+            if (sum > target) {
+                sum -= result.get(result.size() - 1);
+                result.remove(result.size() - 1);
+                backtrack(nums, target, idx++, sum);
+            }
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return results;
+    }
+}
+
+*/
+
+class Solution {
+    List<List<Integer>> results = new ArrayList<>();
+    List<Integer> result = new ArrayList<>();
+
+    private void backtrack(int[] nums, int target, int idx, int sum) {
+        if (idx < nums.length) {
+            if (sum == target) {
+                List<Integer> temp = new ArrayList<>(result);
+                results.add(temp);
+            }
+            if (sum < target) {
+                result.add(nums[idx]);
+                backtrack(nums, target, idx, sum + nums[idx]);
+
+                result.remove(result.size() - 1);
+                backtrack(nums, target, idx + 1, sum);
+            }
+        }
+    }
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return results;
+    }
+}
