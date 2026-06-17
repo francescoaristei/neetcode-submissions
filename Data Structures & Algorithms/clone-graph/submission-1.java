4e7a242e7a4ab4a9369dc266bace888c41a37b84
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-06-17 16:05:11 +0200

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.java
@@ -0,0 +1,41 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private Map<Node, Node> oldToNew = new HashMap<>();
+
+    private void dfs(Node node) {
+        if (!oldToNew.containsKey(node)) {
+            oldToNew.put(node, new Node(node.val));
+            for (Node ne: node.neighbors) {
+                dfs(ne);
+                oldToNew.get(node).neighbors.add(oldToNew.get(ne));
+            }
+        }
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        dfs(node);
+        return oldToNew.get(node);
+    }
+}
