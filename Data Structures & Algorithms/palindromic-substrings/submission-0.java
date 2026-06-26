17851ad894c95a676d93bab70a88170428a6acd6
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-06-26 16:12:16 +0200

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int num = 0;
+        int n = s.length();
+        boolean[][] memo = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) &&
+                    (j - i < 2 || memo[i + 1][j - 1])) {
+                        memo[i][j] = true;
+                        num += 1;
+                    }
+            }
+        }
+        return num;
+    }
+}
