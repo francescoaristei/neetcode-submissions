0e818001106e5f30b6838ef3920040ff645d8976
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-07-02 10:11:13 +0200

    Add: maximum-product-subarray - submission-10

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-10.java
@@ -0,0 +1,27 @@
+// also study the kadane algorithm for max sum in subarray
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int maxProd = Integer.MIN_VALUE;
+        int lTr = 1;
+        int rTl = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (lTr == 0) {
+                lTr = 1;
+            }
+            if (rTl == 0) {
+                rTl = 1;
+            }
+
+            lTr *= nums[i];
+
+            int j = nums.length - i - 1;
+            rTl *= nums[j];
+
+            maxProd = Math.max(lTr, Math.max(maxProd, rTl));
+        }
+
+        return maxProd;
+    }
+}
