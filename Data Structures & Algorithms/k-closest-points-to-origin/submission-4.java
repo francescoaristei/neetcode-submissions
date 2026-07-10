44d6301944890ff9b11d07b718dea28f11df8b88
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-07-10 12:29:37 +0200

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    record Entry(Double d, Integer i, Integer j) implements Comparable<Entry> {
+        @Override
+        public int compareTo(Entry other) {
+            return Double.compare(this.d, other.d);
+        }
+    };
+
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Entry> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < points.length; i++) {
+            double distance = Math.sqrt(Math.pow(points[i][0], 2) + Math.pow(points[i][1], 2));
+            Entry entry = new Entry(distance, points[i][0], points[i][1]);
+            pq.offer(entry);
+        }
+
+        int[][] result = new int[k][2];
+
+        int counter = k - 1;
+        while (counter >= 0) {
+            Entry entry = pq.poll();
+            result[counter][0] = entry.i;
+            result[counter][1] = entry.j;
+            counter--;
+        }
+
+        return result;
+    }
+}
