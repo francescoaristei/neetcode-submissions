344818039bbe5d42ba92a0621ba65460082f1287
Francesco Aristei <34400161+francescoaristei@users.noreply.github.com>
2026-08-21 15:04:16 +0200

    Add: hand-of-straights - submission-7

---
+++ Data Structures & Algorithms/hand-of-straights/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand.length % groupSize != 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num: hand) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>(count.keySet());
+
+        while (!heap.isEmpty()) {
+            int min = heap.peek();
+            for (int i = min; i < min + groupSize; i++) {
+                if (!count.containsKey(i)) {
+                    return false;
+                }
+                count.put(i, count.get(i) - 1);
+                if (count.get(i) == 0) {
+                    if (i != heap.peek()) {
+                        return false;
+                    }
+                    heap.poll();
+                }
+            }
+        }
+        return true;
+    }
+}
